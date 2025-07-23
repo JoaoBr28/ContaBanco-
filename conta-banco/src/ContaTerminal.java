b41b5b322839d34235fdfde02cbc309888b9d344
joaoBr <joaokamikaze1234@gmail.com>
2025-07-22 23:24:29 -0300

    inicio de versao

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,31 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        
-        double saldo = 25;
-        double valorSolicitado = 18;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(" Primeira tentativa de saque ");
-        if (saldo >= valorSolicitado) {
-            saldo -= valorSolicitado;
-            System.out.println("Saque realizado com sucesso.");
-        } else {
-            System.out.println("Saldo insuficiente.");
-        }
-        System.out.println("Saldo atual: R$ " + saldo);
+        // Solicita e lê o número da conta
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
 
-    
-        
-        saldo = 15;
-        valorSolicitado = 22;
+        // Solicita e lê a agência
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
 
-        System.out.println("\n### Segunda tentativa de saque ###");
-        if (saldo >= valorSolicitado) {
-            saldo -= valorSolicitado;
-            System.out.println("Saque realizado com sucesso.");
-        } else {
-            System.out.println("Saldo insuficiente.");
-        }
-        System.out.println("Saldo atual: R$ " + saldo);
+        // Solicita e lê o nome do cliente
+        System.out.print("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        // Solicita e lê o saldo
+        System.out.print("Por favor, digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        // Mensagem final
+        System.out.println();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
